839c10788cf1168f302863dfa19c27e9b44c3a10
Alexandre Neto <alexandreneto.dev@gmail.com>
2025-12-02 01:21:55 -0300

    #10 - Improve error handling in Express app
    Replaces incorrect mongoose import, refines error middleware to handle CastError and ValidationError specifically, and updates error responses for better clarity. Also reorders middleware and route setup for improved structure.

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import connectDB from "./config/dbConnect.js";
 import routes from "./routes/index.js";
 import { swaggerUi, swaggerSpec } from "./config/swaggerConfig.js";
-import { mongo } from "mongoose";
+import mongoose from "mongoose";
 
 const dbConnection = await connectDB();
 dbConnection.on(
@@ -15,22 +15,26 @@ dbConnection.once("open", () => console.log("MongoDB connected!"));
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
-
-app.use((err, req, res, next) => {
-  if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
-    return res.status(400).send({ message: "Invalid JSON payload" });
-  }
-
-  if (err instanceof mongo.Error.CastError) {
-    return res.status(400).send({ message: "Something is wrong" });
-  }
-  res.status(500).send("Something broke!");
-});
 
 routes(app);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.use((err, req, res, next) => {
+  console.log("Middleware de erro pegou:", err);
+
+  if (err instanceof mongoose.Error.CastError) {
+    return res.status(400).send({ message: "Something is wrong" });
+  } else if (err instanceof mongoose.Error.ValidationError) {
+    return res
+      .status(400)
+      .send({ message: `The following errors were found: ${err.message}` });
+  } else {
+    return res.status(500).json({
+      error: "Internal Server Error",
+    });
+  }
+});
 
 export default app;
